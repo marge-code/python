43c30b181835fdc434eda0721d2c247f76648921
marge-code <mgritsenko@gmail.com>
2016-08-09 18:14:30 +0300

    Binary Search

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -1,30 +1,29 @@
-def search_rec(nums, key, left, right):
-	if left == right:
-		return None
-	middle = (left + right) / 2
-	if key == nums[middle]:
-		return middle
-	else:
-		if key < nums[middle]:
-			return search_rec(nums, key, left, middle - 1)
-		elif key > nums[middle]:
-			return search_rec(nums, key, middle + 1, right)
+import random
 
-def search_iter(nums, key):
-	left = 0
-	right = len(nums) - 1
-	while True:
-		if left == right:
-			break
-		middle = (left + right) / 2
-		if key == nums[middle]:
-			return middle
-		elif key < nums[middle]:
-			right = middle - 1
-		elif key > middle:
-			left = middle + 1
+def binary_search(nums, key):
+    return _binary_search_impl(nums, key, 0, len(nums))
 
-nums = [1, 2, 3, 4, 5, 6, 7, 9]
+def _binary_search_impl(nums, key, left, right):
+    if left >= right:
+        return None
 
-#print search_rec(nums, 100, 0, len(nums))
-print search_iter(nums, 100)
+    while left < right:
+        middle  = (left + right) / 2
+        if nums[middle] == key:
+            return middle
+        elif key < nums[middle]:
+            right = middle
+        elif key > nums[middle]:
+            left = middle + 1
+
+def test():
+    assert binary_search([], 5) == None
+    assert binary_search([100], 100) == 0
+    assert binary_search([1,2,3,4,5], 5) == 4
+    assert binary_search([1,2,3,4,5], 1) == 0
+    test_nums = range(100)
+    key = random.randint(0, 99)
+    assert binary_search(test_nums, key) == test_nums.index(key)
+
+if __name__ == 'main':
+    test()
