04ae671fa26ebfc48b65e18bf1290c6cb2d01a9e
marge-code <mgritsenko@gmail.com>
2016-08-09 20:08:46 +0300

    Fix _binary_search_impl, add tests

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -4,7 +4,7 @@ def binary_search(nums, key):
     return _binary_search_impl(nums, key, 0, len(nums))
 
 def _binary_search_impl(nums, key, left, right):
-    if left >= right:
+    if left > right:
         return None
 
     while left < right:
@@ -16,14 +16,20 @@ def _binary_search_impl(nums, key, left, right):
         elif key > nums[middle]:
             left = middle + 1
 
+    return None
+
 def test():
-    assert binary_search([], 5) == None
-    assert binary_search([100], 100) == 0
-    assert binary_search([1,2,3,4,5], 5) == 4
-    assert binary_search([1,2,3,4,5], 1) == 0
+    print ':)'
+    assert binary_search([], key=5) == None
+    assert binary_search([1], key=5) == None
+    assert binary_search([1, 2, 3], key=4) == None
+    assert binary_search([1, 2], key=1) == 0
+    assert binary_search([1, 2], key=2) == 1
+    assert binary_search([1,2,3,4,5], key=5) == 4
+    assert binary_search([1,2,3,4,5], key=1) == 0
     test_nums = range(100)
     key = random.randint(0, 99)
     assert binary_search(test_nums, key) == test_nums.index(key)
 
-if __name__ == 'main':
+if __name__ == '__main__':
     test()
